603ed4d0d20b1def0a10c2febbd4e3727db0ada6
lucastl1000 <lucassendsbullet@gmail.com>
2025-06-27 07:53:11 -0400

    rota Alterar e deletar do cliente.

--- 3-server-express-api/routes/cliente.js
+++ 3-server-express-api/routes/cliente.js
@@ -38,14 +38,66 @@ routes.post('/', async(req, res) => {
 
 // PUT (Update)
 // Rota para atualizar (Update) valores no BD
-routes.put('/', (req, res) => {
-  res.status(200).send('/cliente (PUT)');
+routes.put('/:id', async(req, res) => {
+  const { id } = req.params;
+
+  if(!id){
+    return res.status(400).json({
+      mensagem: 'O id precisa ser informado'});
+  }
+
+  const {nome, email, telefone, endereco, cidade, uf} = req.body;
+
+  if(!nome || !email || !telefone || !endereco || !cidade || !uf){
+    return res.status(400).json({mensagem:'Todos os campos são obrigatórios.'});
+  }
+
+  const sql = `
+    UPDATE cliente
+    SET nome = $1, email = $2, telefone = $3, endereco = $4, cidade = $5, uf = $6
+    WHERE id = $7
+    RETURNING *
+  `;
+
+  const valores = [nome, email, telefone, endereco, cidade, uf, id];
+
+  const result = await db.query(sql, valores);
+
+  if(result.rows.length === 0){
+    return res.status(404).json({
+      mensagem: 'Cliente não encontrado.'});
+  }
+
+  res.status(200).json(result.rows[0]);
 });
 
 // DELETE (Delete)
 // Rota para excluir (Delete) valores do BD
-routes.delete('/', (req, res) => {
-  res.status(200).send('/cliente (DELETE)');
+routes.delete('/:id', async ( req, res) => {
+  const { id } = req.params;
+
+  if(!id){
+    return res.status(400).json({
+      mensagem: 'O id precisa ser informado'});
+  }
+
+  const sql = `
+  DELETE FROM cliente
+  WHERE id = $1
+  RETURNING *
+  `;
+
+  const valores = [id];
+
+  const result = await db.query(sql, valores);
+
+  if(result.rows.length === 0){
+    return res.status(404).json({
+      mensagem: 'Cliente não encontrado.'});
+  }
+
+  res.status(200).json({
+    mensagem: `Cliente com ID ${id} foi excluído com sucesso`});
 });
 
 // Exportar o módulo com as rotas
